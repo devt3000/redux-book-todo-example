a1086da67c0513b8d3424f8607ba5532805e94f5
Stanley Makazhe <devt3000@gmail.com>
2018-09-06 15:52:17 +0200

    Adding the store, actions and reducers

---
+++ app.js
@@ -0,0 +1,39 @@
+import { createStore } from "redux";
+
+const initialState = {
+  recipes: [
+    {
+      name: "Omelette"
+    }
+  ],
+  ingredients: [
+    {
+      recipe: "Omelette",
+      name: "Egg",
+      quantity: 2
+    }
+  ]
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_RECIPE":
+      return Object.assign({}, state, {
+        recipes: state.recipes.concat({ name: action.name })
+      });
+
+    case "ADD_INGREDIENT":
+      const newIngredient = {
+        name: action.name,
+        recipe: action.recipe,
+        quantity: action.quantity
+      };
+      return Object.assign({}, state, {
+        ingredients: state.ingredients.concat(newIngredient)
+      });
+  }
+  return state;
+};
+const store = createStore(reducer, initialState);
+
+window.store = store;

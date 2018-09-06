6d893bb8b1a992aeadc9782cfbe46b1fe4701404
Stanley Makazhe <devt3000@gmail.com>
2018-09-06 16:15:56 +0200

    Using the new file structure config

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
-import { createStore } from "redux";
+import store from './store';
+import { addRecipe } from './actions/recipes';
+import { addIngredient } from './actions/ingredients';
 
-const initialState = {
-  recipes: [
-    {
-      name: "Omelette"
-    }
-  ],
-  ingredients: [
-    {
-      recipe: "Omelette",
-      name: "Egg",
-      quantity: 2
-    }
-  ]
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "ADD_RECIPE":
-      return Object.assign({}, state, {
-        recipes: state.recipes.concat({ name: action.name })
-      });
-
-    case "ADD_INGREDIENT":
-      const newIngredient = {
-        name: action.name,
-        recipe: action.recipe,
-        quantity: action.quantity
-      };
-      return Object.assign({}, state, {
-        ingredients: state.ingredients.concat(newIngredient)
-      });
-  }
-  return state;
-};
-const store = createStore(reducer, initialState);
-
-window.store = store;
+store.dispatch(addRecipe('Pancakes'));
+store.dispatch(addIngredient('Pancakes', 'Egg', 3));
